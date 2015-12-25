8a87d78345525b130440339915f31b0529c990cd
Qiang Wang <wqpod2g@gmail.com>
2015-12-25 22:38:55 +0800

    1.1

--- LiveEarth.py
+++ LiveEarth.py
@@ -4,7 +4,9 @@ import time
 import string
 import os
 
-base_url = "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/1d/550/"
+base_url = "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/1d/550/" #官网图片地址前半部分
+
+cwd = os.getcwd() #当前目录
 
 def set_desktop(pic_path):
         k = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER,"Control Panel\\Desktop",0,win32con.KEY_SET_VALUE)
@@ -12,7 +14,7 @@ def set_desktop(pic_path):
         win32api.RegSetValueEx(k, "TileWallpaper", 0, win32con.REG_SZ, "0")
         win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER,pic_path, 1+2)
         
-
+#获取当前图片url
 def getPic_url():
         date = time.strftime('%Y/%m/%d',time.localtime(time.time()))
         hour = int(time.strftime('%H',time.localtime(time.time())))-1
@@ -23,16 +25,18 @@ def getPic_url():
 
         if(hour_url<100000):
                 hour_url = "0"+str(hour_url)
-
+        else:
+                hour_url = str(hour_url)
         pic_url = base_url+date+"/"+hour_url+"_0_0.png"
         print(pic_url)
         return pic_url
 
+#下载图片
 def down_pic(pic_url):
         conn = urllib.request.urlopen(pic_url)
         date = time.strftime('%Y-%m-%d',time.localtime(time.time()))
         hour = str(int(time.strftime('%H',time.localtime(time.time())))-1)
-        pic_name = os.getcwd()+"\\downloads\\"+date+"-"+hour+".png"
+        pic_name = cwd+"\\downloads\\"+date+"-"+hour+".png"
         conn = urllib.request.urlopen(pic_url)  
         f = open(pic_name,'wb')
         f.write(conn.read())  
@@ -41,12 +45,12 @@ def down_pic(pic_url):
         return pic_name
         
 def main():
+        if not os.path.exists(cwd+"/downloads"):
+                os.mkdir(cwd+"/downloads") 
         while True:
                 pic_url = getPic_url()
                 pic_name = down_pic(pic_url)
                 set_desktop(pic_name)
                 time.sleep(3600)
-
 main()
-                
 
